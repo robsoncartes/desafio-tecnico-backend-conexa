fa2e2a05b7cbb15de5e5f49106e9cc2ca9cb6394
Robson Sousa <robsoncartes@outlook.com>
2021-08-29 20:04:19 -0300

    Create new classes: 'Patient' and 'PatientView'.

---
+++ src/main/java/br/com/conexasaude/models/Patient.java
@@ -0,0 +1,63 @@
+package br.com.conexasaude.models;
+
+import br.com.conexasaude.models.views.PatientView;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "patients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(PatientView.PatientSimple.class)
+    private Long id;
+
+    @JsonView({PatientView.PatientComplete.class})
+    private String name;
+
+    @CPF
+    @JsonView({PatientView.PatientComplete.class})
+    private String cpf;
+
+    @JsonView({PatientView.PatientComplete.class})
+    private String idade;
+
+    @JsonView({PatientView.PatientComplete.class})
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Patient)) return false;
+        Patient patient = (Patient) o;
+        return id.equals(patient.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", idade='" + idade + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/br/com/conexasaude/models/views/PatientView.java
@@ -0,0 +1,12 @@
+package br.com.conexasaude.models.views;
+
+public class PatientView {
+
+    public static class PatientSimple {
+
+    }
+
+    public static class PatientComplete extends PatientSimple {
+
+    }
+}
