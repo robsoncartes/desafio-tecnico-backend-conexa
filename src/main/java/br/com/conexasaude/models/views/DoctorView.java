623fe8ec15da5913f983fd0dc359f889a84f6c76
Robson Sousa <robsoncartes@outlook.com>
2021-08-28 23:10:49 -0300

    'DoctorView' implemented.

---
+++ src/main/java/br/com/conexasaude/models/views/DoctorView.java
@@ -0,0 +1,12 @@
+package br.com.conexasaude.models.views;
+
+public class DoctorView {
+
+    public static class DoctorLogin {
+
+    }
+
+    public static class DoctorComplete extends DoctorLogin {
+
+    }
+}
